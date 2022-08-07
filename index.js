d441e9d5406514d13dc31e2f659043b9b86c284f
aspencass <caspenw1@gmail.com>
2022-08-07 14:23:46 -0600

    commit tree 57d686b2042c1c5313243de8d9af9c03046d298c

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(rideFeet) {
+  if (rideFeet <= 400) {
+      return 'This one is on me!'
+  } else if (rideFeet > 400 && rideFeet <= 2000) {
+      return 'That will be twenty bucks.'
+  } else if (rideFeet > 2000 && rideFeet <= 2500) {
+      return 'I will gladly take your thirty bucks.'
+  } else (rideFeet > 2500 )
+    return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+      return 'Ok, sounds good.'
+  } else {
+      return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipEvent){
+  if (tipEvent === 'generous') {
+      return 'Thank you so much.'
+  } else if (tipEvent === 'not as generous') {
+      return 'Thank you.'
+  } else (tipEvent === 'thanks for everything')
+      return 'Bye.'
 }
